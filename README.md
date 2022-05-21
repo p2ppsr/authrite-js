425c24ebfc04a2003dd5700f839f3dfd4ae79b53
Ty Everett <ty@tyweb.us>
2022-05-21 12:44:09 -0700

    Improved error forwarding, return status

--- README.md
+++ README.md
@@ -60,12 +60,12 @@ init()
 
 #### Table of Contents
 
-*   [Authrite](#authrite)
+*   [constructor](#constructor)
     *   [Parameters](#parameters)
-    *   [request](#request)
-        *   [Parameters](#parameters-1)
+*   [request](#request)
+    *   [Parameters](#parameters-1)
 
-### Authrite
+### constructor
 
 Client-side API for establishing authenticated server communication
 
@@ -78,15 +78,14 @@ Client-side API for establishing authenticated server communication
     *   `authrite.initialRequestPath` **[String](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/String)** Initial request path for establishing a connection (optional, default `'/authrite/initialRequest'`)
     *   `authrite.initialRequestMethod` **[String](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/String)** Initial request method (optional, default `'POST'`)
 
-#### request
+### request
 
-##### Parameters
+#### Parameters
 
 *   `routePath` **[String](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/String)** The path on the server to request
-*   `fetchConfig` **[object](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Object)** Config object passed to the [Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API).
-    The current version of Authrite only supports JSON structures for the fetch body. However, you can include a [Buffer](https://nodejs.org/api/buffer.html) as part of the json object. (optional, default `{}`)
+*   `fetchConfig` **[object](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Object)** Config object passed to the [Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API). The current version of Authrite only supports JSON structures for the fetch body. However, you can include a [Buffer](https://nodejs.org/api/buffer.html) as part of the json object. (optional, default `{}`)
 
-Returns **[object](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Object)** The response object. Fields are 'headers' and 'body' (containing messageToVerify)
+Returns **[object](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Object)** The response object. Fields are 'status', 'headers' and 'body' (containing an ArrayBuffer of the HTTP response body)
 
 ## License
 
--- src/authrite.js
+++ src/authrite.js
@@ -17,6 +17,7 @@ const AUTHRITE_VERSION = '0.1'
 /**
    * The client requesting communication with the server
    * @param {String} privateKey associated with the identityPublicKey
+   * @private
    */
 class Client {
   constructor (privateKey) {
@@ -34,6 +35,7 @@ class Client {
    * @param {String} nonce A 256 bit number converted to a base64 string
    * @param {Array} certificates Certificate authorities of the Server
    * @param {Object} requestedCertificates Indicates certificates requested by Client
+   * @private
    */
 class Server {
   constructor (baseUrl, identityPublicKey, nonce, certificates, requestedCertificates) {
@@ -45,16 +47,17 @@ class Server {
   }
 }
 
-/**
-    * Client-side API for establishing authenticated server communication
-    * @public
-    * @param {object} authrite All parameters are given in an object.
-    * @param {String} authrite.baseUrl The server baseUrl we want to talk to
-    * @param {String} authrite.clientPrivateKey The client's private key used for derivations
-    * @param {String} authrite.initialRequestPath Initial request path for establishing a connection
-    * @param {String} authrite.initialRequestMethod Initial request method
-    */
 class Authrite {
+  /**
+   * Client-side API for establishing authenticated server communication
+   * @public
+   * @param {object} authrite All parameters are given in an object.
+   * @param {String} authrite.baseUrl The server baseUrl we want to talk to
+   * @param {String} authrite.clientPrivateKey The client's private key used for derivations
+   * @param {String} authrite.initialRequestPath Initial request path for establishing a connection
+   * @param {String} authrite.initialRequestMethod Initial request method
+   * @constructor
+   */
   constructor ({
     baseUrl,
     clientPrivateKey,
@@ -120,9 +123,8 @@ class Authrite {
    * @public
    * Creates a new signed authrite request
    * @param {String} routePath The path on the server to request
-   * @param {object} fetchConfig Config object passed to the [Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API). 
-   * The current version of Authrite only supports JSON structures for the fetch body. However, you can include a [Buffer](https://nodejs.org/api/buffer.html) as part of the json object.
-   * @returns {object} The response object. Fields are 'headers' and 'body' (containing messageToVerify)
+   * @param {object} fetchConfig Config object passed to the [Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API). The current version of Authrite only supports JSON structures for the fetch body. However, you can include a [Buffer](https://nodejs.org/api/buffer.html) as part of the json object.
+   * @returns {object} The response object. Fields are 'status', 'headers' and 'body' (containing an ArrayBuffer of the HTTP response body)
    */
   async request (routePath, fetchConfig = {}) {
     // Check for server parameters
@@ -139,89 +141,66 @@ class Authrite {
       invoiceNumber: 'authrite message signature-' + requestNonce + ' ' + this.server.nonce,
       returnType: 'hex'
     })
-    let response
-    try {
-      // Make sure the fetchConfig body is formatted to the correct content type
-      let dataToSign
-      if (fetchConfig.body) {
-        fetchConfig.body = typeof (fetchConfig.body) === 'string' ? fetchConfig.body : JSON.stringify(fetchConfig.body)
-        dataToSign = fetchConfig.body
-        // TODO: Check fetchConfig.headers['Content-Type'] to support other data types
-        fetchConfig.headers ??= {}
-        fetchConfig.headers['Content-Type'] ??= 'application/json'
-      }
-      // If the request body is empty, sign the url instead
-      dataToSign ??= this.server.baseUrl + routePath
-      // Configure default method and headers if none are provided
-      fetchConfig.method ??= 'POST'
-      // Create a request signature
-      const requestSignature = bsv.crypto.ECDSA.sign(
-        bsv.crypto.Hash.sha256(Buffer.from(dataToSign)),
-        bsv.PrivateKey.fromHex(derivedClientPrivateKey)
-      )
-      // Send the signed Authrite fetch request with the HTTP headers according to the specification
-      response = await fetch(
-        this.server.baseUrl + routePath,
-        {
-          ...fetchConfig,
-          headers: {
-            ...fetchConfig.headers,
-            'X-Authrite': AUTHRITE_VERSION,
-            'X-Authrite-Identity-Key': this.client.publicKey,
-            'X-Authrite-Nonce': requestNonce,
-            'X-Authrite-YourNonce': this.server.nonce,
-            'X-Authrite-Certificates': this.client.certificates,
-            'X-Authrite-Signature': requestSignature.toString()
-          }
-        }
-      )
-      if (!response) {
-        throw new Error('Failed to get response from server!')
-      }
-      // Provide better error messages depending on the status code
-      let customErrorMessage
-      switch (response.status) {
-        case 404:
-          customErrorMessage = `Route ${response.statusText}`
-          break
-        default:
-          customErrorMessage = response.statusText
-      }
-      if (response.status !== 200 && response.status) {
-        throw new Error(customErrorMessage)
-      }
-    } catch (error) {
-      throw new Error(`FetchConfig not configured correctly! ErrorMessage: ${error.message}`)
+    // Make sure the fetchConfig body is formatted to the correct content type
+    let dataToSign
+    if (fetchConfig.body) {
+      fetchConfig.body = typeof (fetchConfig.body) === 'string' ? fetchConfig.body : JSON.stringify(fetchConfig.body)
+      dataToSign = fetchConfig.body
+      // TODO: Check fetchConfig.headers['Content-Type'] to support other data types
+      fetchConfig.headers ??= {}
+      fetchConfig.headers['Content-Type'] ??= 'application/json'
     }
-    try {
-      // When the server response comes back, validate the signature according to the specification
-      const signingPublicKey = getPaymentAddress({
-        senderPrivateKey: this.client.privateKey,
-        recipientPublicKey: this.server.identityPublicKey,
-        invoiceNumber: 'authrite message signature-' + requestNonce + ' ' + response.headers.get('X-Authrite-Nonce'),
-        returnType: 'publicKey'
-      })
-      // 2. Construct the message for verification
-      const messageToVerify = await response.arrayBuffer()
-      // 3. Verify the signature
-      const signature = bsv.crypto.Signature.fromString(
-        response.headers.get('x-authrite-signature')
-      )
-      const verified = bsv.crypto.ECDSA.verify(
-        bsv.crypto.Hash.sha256(Buffer.from(messageToVerify)),
-        signature,
-        bsv.PublicKey.fromString(signingPublicKey)
-      )
-      if (verified) {
-        return {
-          headers: response.headers,
-          body: messageToVerify
+    // If the request body is empty, sign the url instead
+    dataToSign ??= this.server.baseUrl + routePath
+    // Configure default method and headers if none are provided
+    fetchConfig.method ??= 'POST'
+    // Create a request signature
+    const requestSignature = bsv.crypto.ECDSA.sign(
+      bsv.crypto.Hash.sha256(Buffer.from(dataToSign)),
+      bsv.PrivateKey.fromHex(derivedClientPrivateKey)
+    )
+    // Send the signed Authrite fetch request with the HTTP headers according to the specification
+    const response = await fetch(
+      this.server.baseUrl + routePath,
+      {
+        ...fetchConfig,
+        headers: {
+          ...fetchConfig.headers,
+          'X-Authrite': AUTHRITE_VERSION,
+          'X-Authrite-Identity-Key': this.client.publicKey,
+          'X-Authrite-Nonce': requestNonce,
+          'X-Authrite-YourNonce': this.server.nonce,
+          'X-Authrite-Certificates': this.client.certificates,
+          'X-Authrite-Signature': requestSignature.toString()
         }
-      } else {
-        throw new Error('Unable to verify server response')
       }
-    } catch (error) {
-      throw new Error(`Server could not find Authrite headers in request from client! Error Message: ${error}`)
+    )
+    // When the server response comes back, validate the signature according to the specification
+    const signingPublicKey = getPaymentAddress({
+      senderPrivateKey: this.client.privateKey,
+      recipientPublicKey: this.server.identityPublicKey,
+      invoiceNumber: 'authrite message signature-' + requestNonce + ' ' + response.headers.get('X-Authrite-Nonce'),
+      returnType: 'publicKey'
+    })
+    // 2. Construct the message for verification
+    const messageToVerify = await response.arrayBuffer()
+    // 3. Verify the signature
+    const signature = bsv.crypto.Signature.fromString(
+      response.headers.get('x-authrite-signature')
+    )
+    const verified = bsv.crypto.ECDSA.verify(
+      bsv.crypto.Hash.sha256(Buffer.from(messageToVerify)),
+      signature,
+      bsv.PublicKey.fromString(signingPublicKey)
+    )
+    if (verified) {
+      return {
+        status: response.status,
+        headers: response.headers,
+        body: messageToVerify
+      }
+    } else {
+      throw new Error('Unable to verify server response')
     }
   }
 }
